db5fae34e37f7fe2148dda92e317f7141ad7101b
George192004 <giorgiqajaia173@yahoo.com>
2024-02-18 18:10:33 +0400

    Initial Commit

---
+++ Deploy/DeployApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeployApp.swift
+//  Deploy
+//
+//  Created by George on 18.02.24.
+//
+
+import SwiftUI
+
+@main
+struct DeployApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
