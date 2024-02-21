a0d06a510ead699b55f9ae2121660c4e07db8ebd
George192004 <giorgiqajaia173@yahoo.com>
2024-02-21 16:23:50 +0400

    Updated code(NEW)

---
+++ Deploy/MoreView.swift
@@ -0,0 +1,18 @@
+//
+//  MoreView.swift
+//  Deploy
+//
+//  Created by George on 21.02.24.
+//
+
+import SwiftUI
+
+struct MoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoreView()
+}
---
+++ Deploy/MyDocumentsView.swift
@@ -0,0 +1,18 @@
+//
+//  MyDocumentsView.swift
+//  Deploy
+//
+//  Created by George on 21.02.24.
+//
+
+import SwiftUI
+
+struct MyDocumentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyDocumentsView()
+}
