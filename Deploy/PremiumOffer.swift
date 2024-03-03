676683df3c2dfb62cdfc70020918f9e710570b99
George192004 <giorgiqajaia173@yahoo.com>
2024-03-04 00:14:13 +0400

    Updated

--- Deploy/PremiumOffer.swift
+++ Deploy/PremiumOffer.swift
@@ -54,7 +54,7 @@ struct PremiumOffer: View {
             }
             .padding()
 
-            VStack {
+            VStack(spacing: 8) {
                 Image("objects")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
@@ -72,19 +72,25 @@ struct PremiumOffer: View {
                         .font(.system(size:16, weight: .regular))
                         .foregroundColor(Color(red: 0, green: 0, blue: 0, opacity: 0.5))
                 }
+                Spacer()
                 
                 
-                VStack(alignment: .leading, spacing: 16) {
+                
+
+                VStack(alignment: .leading) {
                     GroupOfDealPopUP(icon: "heart", text: "Unlimited document scanner")
                     GroupOfDealPopUP(icon: "printer", text: "Unlimited print")
                     GroupOfDealPopUP(icon: "cloud", text: "Cloud Service")
                     GroupOfDealPopUP(icon: "person", text: "ID Scanner")
                 }
-                .background(Color(red: 244/255, green: 222/255, blue: 199/255, opacity: 0.5))
+                .background(Color(red: 244/255, green: 222/255, blue: 199/255))
                 .cornerRadius(8)
-                .padding(.top, 20)
-                .padding(.bottom, 20)
-                .frame(width: 350)
+                .padding(.top, 43)
+                .padding(.bottom, 53)
+
+                .frame(width: 345)
+                Spacer()
+
                 
                 VStack(spacing: 16){
                     VStack(spacing: 4){
@@ -104,14 +110,11 @@ struct PremiumOffer: View {
                             .foregroundColor(.white)
                             .padding(17)
                             .padding(.horizontal, 100)
-                            .background(Color.black)
-                            .cornerRadius(1000)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 1000)
-                                    .stroke(Color.blue, lineWidth: 1)
-                            )
+                            .background(.black)
+                            .cornerRadius(100)
                     }
                 }
+//                Spacer()
             }
         }
         .onAppear {
@@ -141,21 +144,181 @@ struct GroupOfDealPopUP: View {
     var icon: String
     var text: String
     var body: some View {
-        HStack(spacing: 16) {
+        HStack(alignment: .center, spacing: 16) {
             Image(systemName: icon)
                 .resizable()
-                .aspectRatio(contentMode: .fill)
+       
                 .frame(width: 20, height: 20)
+            
             Text(text)
                 .font(.system(size: 14, weight: .regular))
         }
-        .padding(.leading, 16)
-        .padding(.top, 16)
-        .padding(.bottom, 16)
-        .padding(.trailing, 103)
+        .padding(.horizontal, 16)
+        .padding(.vertical, text == "ID Scanner" ? 16 : 16)
+        
     }
 }
 
 #Preview {
     PremiumOffer(isPresented: .constant(false))
 }
+
+
+
+
+//import SwiftUI
+//
+//struct GrabDealView: View {
+//    @State private var drawBorder: CGFloat = 0
+//    @State private var iconAndBorderColor = Color.black.opacity(0.05)
+//    @Binding var isVisible: Bool
+//    @Environment(\.presentationMode) var presentationMode
+//    var body: some View {
+//        
+//        VStack {
+//            HStack {
+//                Spacer()
+//                Button(action: {
+//                    presentationMode.wrappedValue.dismiss()
+//                    print("X button was tapped")
+//                }) {
+//                    Image(systemName: "xmark")
+//                        .foregroundColor(iconAndBorderColor)
+//                        .frame(width: 32, height: 32)
+//                        .clipShape(Circle())
+//                        .overlay(
+//                            Circle()
+//                                .trim(from: 0, to: drawBorder)
+//                                .stroke(iconAndBorderColor, lineWidth: 3)
+//                                .rotationEffect(.degrees(-90))
+//                        )
+//                }
+//                .onAppear {
+//                    withAnimation(.linear(duration: 3)) {
+//                        drawBorder = 1
+//                    }
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//                        iconAndBorderColor = .black
+//                    }
+//                }
+//            }.padding(.horizontal, 24)
+//            Image("objects")
+//                .resizable()
+//                .aspectRatio(contentMode: .fill)
+//                .frame(width: 253, height: 180)
+//            Text("-50%")
+//                .font(.system(size: 40))
+//                .fontWeight(.bold)
+//                .padding(.bottom, 10)
+//                .foregroundColor(.black)
+//            
+//            Text("Sale only for you")
+//                .font(.system(size: 16))
+//                .fontWeight(.medium)
+//                .foregroundColor(.black.opacity(0.6))
+//                .padding(.bottom, 10)
+//            
+//            Text("This offer expires in 59:08")
+//                .font(.system(size: 16))
+//                .foregroundColor(.black.opacity(0.6))
+//            
+//            VStack {
+//                Spacer()
+//                VStack(alignment: .leading) {
+//                    ForEach([
+//                        ("heart", "Unlimited document scanner"),
+//                        ("printer", "Unlimited print"),
+//                        ("cloud", "Cloud Service"),
+//                        ("person", "ID Scanner"),
+//                    ], id: \.0) { icon, title in
+//                        ItemView(icon: icon, title: title)
+//                    }
+//                    .padding(.bottom, 16)
+//                }
+//                .background(Color.black.opacity(0.06))
+//                .clipShape(RoundedRectangle(cornerRadius: 10))
+//                .padding(.horizontal, 24)
+//                
+//                Spacer()
+//                VStack {
+//                    Text("Just $49.99 19.99/year")
+//                        .fontWeight(.medium)
+//                        .font(.system(size: 16))
+//                        .foregroundColor(.black)
+//                    Text("Auto renewable. Cancel Anytime")
+//                        .font(.system(size: 14))
+//                        .foregroundColor(.black)
+//                }.padding(.top).padding(.bottom)
+//
+//                
+//                Button(action: {
+//                    print("Button tapped!")
+//                }) {
+//                    Text("Grab this deal")
+//                        .fontWeight(.semibold)
+//                        .foregroundColor(.white)
+//                }
+//                .padding(.horizontal, 110)
+//                .padding(.vertical, 13)
+//                .background(Color.black)
+//                .cornerRadius(100)
+//            }
+//            
+//        }
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .background(Color(hex: "#FEEACF"))
+//    }
+//}
+//
+//
+//
+//
+//struct ItemView: View {
+//    let icon: String
+//    let title: String
+//
+//    var body: some View {
+//        HStack {
+//            Image(systemName: icon)
+//                .padding(.leading, 16)
+//                .frame(width: 20, height: 20)
+//                .foregroundColor(.black)
+//            Text(title)
+//                .font(.system(size: 14))
+//                .padding(.leading, 16)
+//                .foregroundColor(.black)
+//            
+//            Spacer()
+//        }
+//        .frame(maxWidth: .infinity, alignment: .leading)
+//        .padding(.leading, 16)
+//        .padding(.top, 16)
+//    }
+//}
+//
+//
+//extension Color {
+//    init(hex: String) {
+//        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+//        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+//
+//        var rgb: UInt64 = 0
+//
+//        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+//
+//        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
+//        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
+//        let blue = Double(rgb & 0x0000FF) / 255.0
+//
+//        self.init(red: red, green: green, blue: blue)
+//    }
+//}
+//
+//
+//                         
+//struct GrabDealView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        GrabDealView(isVisible: .constant(true))
+//
+//    }
+//}
